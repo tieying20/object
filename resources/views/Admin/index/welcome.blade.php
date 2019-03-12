6536c482fc1de858d25a85cab91e1dc6d0935a3d
linshangqing <270899077@qq.com>
2019-03-12 15:57:29 +0800

    修改了欢迎页面

--- resources/views/Admin/index/welcome.blade.php
+++ resources/views/Admin/index/welcome.blade.php
@@ -13,7 +13,7 @@
     <body>
     <div class="x-body layui-anim layui-anim-up">
         <blockquote class="layui-elem-quote">欢迎管理员：
-            <span class="x-red">test</span>！当前时间:2018-04-25 20:50:53</blockquote>
+            <span class="x-red">admin</span>！当前时间:2019-03-12 20:50:53</blockquote>
         <fieldset class="layui-elem-field">
             <legend>数据统计</legend>
             <div class="layui-field-box">
@@ -34,7 +34,7 @@
                                             <a href="javascript:;" class="x-admin-backlog-body">
                                                 <h3>会员数</h3>
                                                 <p>
-                                                    <cite>12</cite></p>
+                                                    <cite>1222</cite></p>
                                             </a>
                                         </li>
                                         <li class="layui-col-xs2">
@@ -80,12 +80,12 @@
                     <tbody>
                         <tr>
                             <td >
-                                <a class="x-a" href="/" target="_blank">新版x-admin 2.0上线了</a>
+                                <a class="x-a" href="/" target="_blank">有个社区 1.0上线了!</a>
                             </td>
                         </tr>
                         <tr>
                             <td >
-                                <a class="x-a" href="/" target="_blank">交流qq群:(519492808)</a>
+                                <a class="x-a" href="/" target="_blank">项目正在积极准备上线当中...</a>
                             </td>
                         </tr>
                     </tbody>
@@ -98,20 +98,20 @@
                 <table class="layui-table">
                     <tbody>
                         <tr>
-                            <th>xxx版本</th>
+                            <th>1.0版本</th>
                             <td>1.0.180420</td></tr>
                         <tr>
                             <th>服务器地址</th>
                             <td>x.xuebingsi.com</td></tr>
                         <tr>
                             <th>操作系统</th>
-                            <td>WINNT</td></tr>
+                            <td>WIN 10</td></tr>
                         <tr>
                             <th>运行环境</th>
                             <td>Apache/2.4.23 (Win32) OpenSSL/1.0.2j mod_fcgid/2.3.9</td></tr>
                         <tr>
                             <th>PHP版本</th>
-                            <td>5.6.27</td></tr>
+                            <td>7.0</td></tr>
                         <tr>
                             <th>PHP运行方式</th>
                             <td>cgi-fcgi</td></tr>
@@ -119,8 +119,8 @@
                             <th>MYSQL版本</th>
                             <td>5.5.53</td></tr>
                         <tr>
-                            <th>ThinkPHP</th>
-                            <td>5.0.18</td></tr>
+                            <th>Laravel</th>
+                            <td>5.5</td></tr>
                         <tr>
                             <th>上传附件限制</th>
                             <td>2M</td></tr>
@@ -141,17 +141,17 @@
                     <tbody>
                         <tr>
                             <th>版权所有</th>
-                            <td>xxxxx(xxxx)
-                                <a href="http://www.xxx.com/" class='x-a' target="_blank">访问官网</a></td>
+                            <td>作战小组(二期)
+                            
                         </tr>
                         <tr>
                             <th>开发者</th>
-                            <td>马志斌(113664000@qq.com)</td></tr>
+                            <td>黎铁英、林尚卿、陈乐飞</td></tr>
                     </tbody>
                 </table>
             </div>
         </fieldset>
-        <blockquote class="layui-elem-quote layui-quote-nm">感谢layui,百度Echarts,jquery,本系统由x-admin提供技术支持。</blockquote>
+        <blockquote class="layui-elem-quote layui-quote-nm">感谢layui,百度Echarts,jquery,本系统由Laravel提供技术支持。</blockquote>
     </div>
     
     </body>
